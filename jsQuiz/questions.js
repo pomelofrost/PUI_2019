0fd51f78fb83d2f7460de876c176610420c25933
pomelofrost <38967354+pomelofrost@users.noreply.github.com>
2019-09-30 11:31:20 -0400

    Update questions.js

--- jsQuiz/questions.js
+++ jsQuiz/questions.js
@@ -17,24 +17,27 @@ class Quiz {
     constructor(questions) {
         this.score = 0;
         this.questions = questions;
-        this.questionIndex = 0;}
+        this.questionIndex = 0;
+    }
      getQuestion() {
-
             return this.questions[this.questionIndex];
-        
         }
+    
+    isCorrectAnswer(choice){
+            return this.answer === choice;
+        }
+
     guess(answer) {
         if(this.getQuestion().isCorrectAnswer(answer)) {
                 this.score++;
                     }
-                
-        this.questionIndex++};
+        this.questionIndex++;
+        }
+            
     isEnded() {
             return this.questionIndex === this.questions.length;
         };
 }
-
-
 
 
 function guess(id, guess) {
@@ -44,8 +47,6 @@ function guess(id, guess) {
    	 populate();
     }
 }
-
-
 
 
 function populate() {
