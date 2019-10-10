317b80040bbf91af92ad242669d1c789d0ac6fc6
pomelofrost <38967354+pomelofrost@users.noreply.github.com>
2019-10-10 16:07:59 -0400

    fix cart page styling

--- assgn6/index.js
+++ assgn6/index.js
@@ -27,10 +27,12 @@ function chocolate(){
 
 function addGlazing(){
     console.log(Bun.flavor, Bun.glazing, Bun.quantity);
+    //clear border styling
     document.getElementById("none").style.border="none";
     document.getElementById("vanilla").style.border="none";
     document.getElementById("sugar").style.border="none";
     document.getElementById("chocolate").style.border="none";
+    //add border to the one seleccted
     document.getElementById(Bun.glazing).style.border="solid 3px black";
     document.getElementById(Bun.glazing).style.opacity="1";
 }
@@ -38,11 +40,11 @@ function addGlazing(){
 function order(){
     var quantitySelect = document.getElementById("quantitySelect").value;
     Bun.quantity = quantitySelect;
-
+    //checks if all inputs are finished
     if(typeof Bun.glazing !== "undefined"){
+        //add to cart
         cart.push([Bun.flavor, Bun.glazing, Bun.quantity]);
     }
-
     document.getElementById("cartNum").innerHTML = cart.length;
     document.getElementById("cartNum").style.display = "inline-block";
 }
