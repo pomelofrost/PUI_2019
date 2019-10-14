35c2077cf0dfb096e1d228f56c3cf9f421ac3425
pomelofrost <38967354+pomelofrost@users.noreply.github.com>
2019-10-14 13:37:13 -0400

    fully function cart

--- assgn6/index.js
+++ assgn6/index.js
@@ -31,8 +31,7 @@ function addGlazing(){
     document.getElementById("vanilla").style.opacity="0.6";
     document.getElementById("sugar").style.opacity="0.6";
     document.getElementById("chocolate").style.opacity="0.6";
-    //add border to the one seleccted, and make opaque
-    document.getElementById(Bun.glazing).style.border="solid 3px black";
+    // make selected option opaque
     document.getElementById(Bun.glazing).style.opacity="1";
 }
 
@@ -57,16 +56,88 @@ function order(){
 function getCart(){
     //transfer cart items to check out page
     var cart = JSON.parse(localStorage.getItem("shoppingCart"));
-    // console.log(cart,cart.length);
+    //display cart num
+    document.getElementById("cartNum").innerHTML = cart.length;
+    document.getElementById("cartNum").style.display = "inline-block";
+    //tabulate display
+    var parentDiv = document.getElementsByClassName("left")[0];
+    parentDiv.innerHTML = "";
+    console.log(cart);
     for (i=0; i<cart.length; i++){
         var flavor = cart[i][0];
         var glazing = cart[i][1];
         var quantity = cart[i][2];
-        console.log(glazing);
+        //add html to each bun//
+        var left = document.getElementsByClassName("left")[0];
+        //create big div
+        var item = document.createElement("div");
+        item.setAttribute("class","product");
+        item.setAttribute("id","item"+i);
+        //add img
+        var imgNode = document.createElement('img');
+        imgNode.setAttribute("src","img/bun.png");
+        //create secondary div
+        var smallDiv = document.createElement('div');
+        //display flavor
+        var productName = document.createElement('h3');
+        productName.innerHTML = flavor;
+        //add a span to group descriptions
+        var desSpan = document.createElement('span');
+        //display glazing
+        var glazingDes = document.createElement('p');
+        glazingDes.innerHTML = "Glazing:";
+        var cartGlazing = document.createElement('p');
+        cartGlazing.innerHTML = glazing;
+        //display quantity select menu
+        var quanSpan = document.createElement('span');
+        //display glazing
+        var quanDes = document.createElement('p');
+        quanDes.innerHTML = "Quantity:";
+        var quantitySelect = document.createElement('select');
+        var option1 = document.createElement('option');
+        var option2 = document.createElement('option');
+        var option3 = document.createElement('option');
+        var option4 = document.createElement('option');
+        option1.innerHTML = "1";
+        option1.setAttribute("value","1");
+        option2.innerHTML = "3";
+        option2.setAttribute("value","3");
+        option3.innerHTML = "6";
+        option3.setAttribute("value","6");
+        option4.innerHTML = "12";
+        option4.setAttribute("value","12");
+        quantitySelect.value = quantity;
        
+        //display delete button
+       var deleteButton = document.createElement("button");
+       var trashIcon = document.createElement("i");
+       trashIcon.setAttribute("class","far fa-trash-alt");
+       deleteButton.setAttribute("onClick","deleteItem(i)");
+
+       //append everything
+        left.appendChild(item);
+        item.appendChild(imgNode);
+        item.appendChild(smallDiv);
+        smallDiv.appendChild(productName);
+        smallDiv.appendChild(desSpan);
+        desSpan.appendChild(glazingDes);
+        desSpan.appendChild(cartGlazing);
+        smallDiv.appendChild(quanSpan);
+        quanSpan.appendChild(quanDes);
+        quanSpan.appendChild(quantitySelect);
+        quantitySelect.appendChild(option1);
+        quantitySelect.appendChild(option2);
+        quantitySelect.appendChild(option3);
+        quantitySelect.appendChild(option4);
+        smallDiv.appendChild(deleteButton);
+        deleteButton.appendChild(trashIcon);
     }
-    //tabulate display
-    document.getElementById("productName").innerHTML = flavor;
-    document.getElementById("cartGlazing").innerHTML = glazing;
-    document.getElementById("quantitySelect").value = quantity;
+}
+
+function deleteItem(index){
+    //remove item from cart
+    var cart = JSON.parse(localStorage.getItem("shoppingCart"));
+    cart.splice(i-1,1);
+    localStorage.setItem("shoppingCart",JSON.stringify(cart));    //repopulate page
+    getCart();
 }
